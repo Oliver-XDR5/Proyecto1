66dd49376b7aade7ffca1aa6dd6166dca4349227
yoanrecinos626-lgtm <yoan.recinos626@gmail.com>
2026-08-31 08:46:52 -0600

    Prueba

---
+++ codigo.js
@@ -0,0 +1,5 @@
+let array = ["Pedro", 45, "Juan", 30, "Maria", 25];
+let animales = ["Perro", "Gato", "Elefante", "Tigre", "León"];
+
+document.writeln(animales) ;
+document.writeln("<h1>Hola</h1>")
